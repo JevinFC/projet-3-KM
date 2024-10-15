2ce1957b110b7d1efbe03012ea9e4ec73da44a61
JevinFC <k.machado06@hotmail.com>
2024-10-15 18:35:50 +0200

    changement en dynamique des boutons filtres

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,14 +1,24 @@
-const reponse = await fetch("http://localhost:5678/api/works");
-const creation = await reponse.json(); 
+let reponse;
+let creation;
+
+try{
+reponse = await fetch("http://localhost:5678/api/works")
+if(!reponse.ok){
+    throw new Error(`${reponse.status} : ${reponse.statusText}`)
+}
+creation = await reponse.json()
+}catch(error){
+    console.log(error)
+}
 
 const boutonTous = document.querySelector(".tous")
-const boutonObjets = document.querySelector(".objets")
-const boutonAppartements = document.querySelector(".appartements")
-const boutonHotelsRestaurants = document.querySelector(".hotels-restaurants")
+const boutonObjets = document.querySelector(".bouton-1")
+const boutonAppartements = document.querySelector(".bouton-2")
+const boutonHotelsRestaurants = document.querySelector(".bouton-3")
 const sectionGallery = document.querySelector(".gallery")
 
 async function genererCreation(creation) {
-    sectionGallery.innerHTML = "";
+    sectionGallery.innerHTML = ""
     for (let i = 0; i < creation.length; i++) {
         const article = creation[i];
         console.log(article)
@@ -26,20 +36,47 @@ async function genererCreation(creation) {
         sectionGallery.appendChild(figureGallery)
     }
 }
-boutonTous.addEventListener("click", async function(){
-    genererCreation(creation)
-})
-boutonObjets.addEventListener("click", async function(){
-    const creationObjets = creation.filter(article => article.categoryId ===1)
-    genererCreation(creationObjets)
-})
 
-boutonAppartements.addEventListener("click", async function(){
-   const creationAppartement = creation.filter(article => article.categoryId ===2)
-   genererCreation(creationAppartement)  
-})
 
-boutonHotelsRestaurants.addEventListener("click", function(){
-    const creationHotelAppartements = creation.filter(article => article.categoryId === 3)
-    genererCreation(creationHotelAppartements)
-})
+
+async function genererBoutons(){
+    try{
+        const reponseCategories = await fetch("http://localhost:5678/api/categories");
+        if(!reponseCategories.ok){
+            throw new Error(`${reponse.status} : ${reponse.statusText}`);
+        }
+        let categories = await reponseCategories.json();
+        let filtres = document.querySelector(".filtres");
+        filtres.innerHTML=" ";
+
+        categories.forEach(category => {
+            const bouton = document.createElement('button');
+            bouton.innerText = category.name;
+            bouton.dataset.id = category.id;
+            bouton.classList.add(`bouton-${category.id}`);
+
+            bouton.addEventListener('click', function () {
+                const imagesFiltrees = creation.filter(work => work.categoryId === category.id);
+                genererCreation(imagesFiltrees);
+            });
+
+            filtres.appendChild(bouton);
+        });
+        
+        const boutonTous = document.createElement('button');
+        boutonTous.innerText = 'Tous';
+        boutonTous.classList.add('tous');
+        filtres.prepend(boutonTous)
+
+        boutonTous.addEventListener("click", async function(){
+        genererCreation(creation)
+        })
+    }catch(error) {
+        console.log(error)
+    }}
+
+
+genererBoutons();
+
+genererCreation(creation);
+
